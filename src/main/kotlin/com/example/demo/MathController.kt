d6bbc197457067e288e71f19072e18d30ab13bfc
Horacio <horacio.junior@ibm.com>
2023-04-26 11:26:23 -0300

    initial structure

---
+++ src/main/kotlin/com/example/demo/MathController.kt
@@ -0,0 +1,12 @@
+package com.example.demo
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.atomic.AtomicLong
+
+@RestController
+class MathController {
+    val counter: AtomicLong = AtomicLong()
+
+}
